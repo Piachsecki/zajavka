4de1794fc6d9cdd2a22b2a4eb35c988d25496aad
Piachsecki <k.r.piasecki2@gmail.com>
2022-11-08 00:21:46 +0100

    Lekcja 36 - ArrayList

---
+++ src/ArrayLista/Main.java
@@ -0,0 +1,32 @@
+package ArrayLista;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList();
+        list.add("Kacper");
+        list.add("Piotrek");
+        list.add("Wiktor");
+        list.add("Hania");
+        System.out.println(list);
+        System.out.println();
+        System.out.println(list.contains("Maciek"));
+        System.out.println(list.contains("Piotrek"));
+        System.out.println();
+        list.remove("Hania");
+        list.remove(0);
+        System.out.println(list);
+        System.out.println();
+        Object[] arr = list.toArray();
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+        System.out.println(list.isEmpty());
+
+    }
+}
